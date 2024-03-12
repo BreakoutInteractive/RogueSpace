28620a5104ccc5244aed6bc00c9c3abbd8f78a48
Dylan McIntyre <dylanrmcintyre@gmail.com>
2024-03-12 15:25:22 -0400

    Enemy Collision Fix
    - Enemies now collide with each other, stopping them from standing on top of each other when pathfinding

--- source/models/Enemy.cpp
+++ source/models/Enemy.cpp
@@ -40,7 +40,7 @@ bool Enemy::init(const Vec2 pos, const Size size) {
     b2Filter filter;
     // this is an enemy and can collide with a player "shadow", an enemy "shadow", a wall, or an attack
     filter.categoryBits = CATEGORY_ENEMY;
-    filter.maskBits = CATEGORY_PLAYER_SHADOW | CATEGORY_WALL | CATEGORY_ATTACK;
+    filter.maskBits = CATEGORY_PLAYER_SHADOW | CATEGORY_ENEMY | CATEGORY_WALL | CATEGORY_ATTACK;
     box->setFilterData(filter);
     _collider = box;
     
