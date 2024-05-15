62de2d14fb3816e8fa86bb8d471af225dbdf5a1b
Zhiyuan Chen <zc333@cornell.edu>
2024-05-15 10:12:25 -0400

    adding overlay, layout to confirmation

--- source/App.cpp
+++ source/App.cpp
@@ -32,12 +32,13 @@ void App::onStartup() {
     
     // Queue the other assets
     AudioEngine::start(24);
+    // must ensure assets are loaded in order of being used in scene graphs!
     _assets->loadDirectoryAsync("json/assets.json",nullptr);
+    _assets->loadDirectoryAsync("json/scenes/title.json", nullptr);
     _assets->loadDirectoryAsync("json/scenes/gameplay.json", nullptr);
     _assets->loadDirectoryAsync("json/scenes/hud.json", nullptr);
     _assets->loadDirectoryAsync("json/scenes/pause.json", nullptr);
     _assets->loadDirectoryAsync("json/scenes/upgrades.json", nullptr);
-    _assets->loadDirectoryAsync("json/scenes/title.json", nullptr);
     _assets->loadDirectoryAsync("json/scenes/settings.json", nullptr);
     _assets->loadDirectoryAsync("json/scenes/death.json", nullptr);
     _assets->loadDirectoryAsync("json/animations/player.json", nullptr);
